f80887c3439dd78a29ee26ed305072eda6ee677c
David Fletcher <davidfletcher@Davids-Air-2.lan>
2021-10-25 16:47:31 -0700

    able to change the input

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,14 +4,26 @@ export default class TodoForm extends Component{
    constructor(){
       super();
       this.state = {
-         todoText: ''
-      }
+         todoText: '',
+      };
    }
+
+   handleChange = (evt) => {
+      const { name, value } = evt.target;
+      this.setState({
+         [name]: value,
+      })
+   }
+
    render(){
       return(
          <form>
             <label htmlFor='todo-input' > Add something ToDo: </label>
-            <input id='todo-input' />
+            <input 
+               id='todo-input' 
+               name='todoText'
+               value={this.state.todoText}
+               onChange={this.handleChange} />
          </form>
       )
    }
